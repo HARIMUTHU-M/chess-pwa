f849ef158ef35d4a4c8cad5c68f24e35985a2be4
Arun <arunmugunthan07@gmail.com>
2022-10-16 18:15:14 +0530

    added ui

--- src/App.js
+++ src/App.js
@@ -5,23 +5,101 @@ import { Game, move, status, moves, aiMove, getFen } from 'js-chess-engine'
 const game = new Game()
 function App() {
   useEffect(() => { 
-    console.log(game.moves());
+    console.log(game);
   }, []);
-  return (
-    <div className=" h-screen text-center " style={{backgroundImage:'url("./images/Background.jpg")'}} >
+  return (  
+    <div className=" h-screen text-center " style={{backgroundImage:'url("./images/Background.jpg")',backgroundRepeat:'no-repeat',backgroundSize:'100% 100%'}} >
       
       <div className='flex h-full'>
          <div className='basis-1/4 left-bar' >
-asdasd
          </div>
-         <div className='basis-1/2 w-fit    board'  >
-          <div className=' w-full h-full ' style={{backgroundImage:'url("./images/chessboard-02.png")',backgroundRepeat:'no-repeat', backgroundPosition:'center' , backgroundSize:'auto 100%'}} >
-
+         <div className='basis-1/2 w-fit px-20   board'  >
+          <div className=' w-full h-full pl-[12%] pr-[13%] pt-[5.9rem] py-[15.4rem] ' style={{backgroundImage:'url("./images/chessboard-02.png")',backgroundRepeat:'no-repeat', backgroundPosition:'center' , backgroundSize:'100% 100%'}} >
+            <div className='bg-rd-400 h-[100%]  flex-col  ' >
+              <div className='flex h-[12.5%] relative flex-auto w-full ' >
+                <div className='flex-auto' ><img src='./images/p.png' className='h-40 top-[-6.5rem] left-[0rem] absolute  '   ></img>  </div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto relative'><img src='./images/i.png' className='h-40 top-[-6.5rem] left-[0rem] absolute '/></div>
+                <div className='flex-auto relative'><img src='./images/b.png' className='h-40 top-[-6.5rem] left-[0rem] absolute '/></div>
+                <div className='flex-auto relative'><img src='./images/q.png' className='h-40 top-[-6.5rem] left-[0rem] absolute '/></div>
+                <div className='flex-auto relative'><img src='./images/r.png' className='h-40 top-[-6.5rem] left-[0rem] absolute '/></div>
+                <div className='flex-auto relative'><img src='./images/k.png' className='h-40 top-[-6.5rem] left-[0rem] absolute '/></div>
+              </div>
+              <div className='flex h-[12.5%] relative flex-auto w-full ' >
+                <div className='flex-auto bg-green-400 z-10'>  </div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto relative'><img src='./images/k.png' className='h-40 top-[-6.5rem] left-[0rem] absolute '/></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+              </div>
+              <div className='flex h-[12.5%]  flex-auto w-full ' >
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+              </div>
+              <div className='flex h-[12.5%]  flex-auto w-full ' >
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+              </div>
+              <div className='flex h-[12.5%]  flex-auto w-full ' >
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+              </div>
+              <div className='flex h-[12.5%]  flex-auto w-full ' >
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+              </div>
+              <div className='flex h-[12.5%]  flex-auto w-full ' >
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+              </div>
+              <div className='flex h-[12.5%]  flex-auto w-full ' >
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+                <div className='flex-auto'></div>
+              </div>
+            </div>
           </div>
-asadsa
          </div>
          <div className='basis-1/4 right-bar' >
-asdas 
          </div>
       </div>
     </div>
@@ -29,3 +107,13 @@ asdas
 }
 
 export default App;
+
+
+// var Object = {
+//   a : [1,2,3,4,5],
+//   b : [1,23,4,5,6]
+// } 
+
+// Object['a'][2] = 5 ;
+// Object['a' ] = [...Object['a']]
+// setObject(Object)
