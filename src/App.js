7e620017e1fcf0a9c5e9784493f3017862b5d171
Arun <arunmugunthan07@gmail.com>
2022-10-16 11:07:04 +0530

    created project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Game, move, status, moves, aiMove, getFen } from 'js-chess-engine'
+const game = new Game()
 function App() {
+  useEffect(() => { 
+    console.log(game.moves());
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" h-screen text-center " style={{backgroundImage:'url("./images/Background.jpg")'}} >
+      
+      <div className='flex h-full'>
+         <div className='basis-1/4 left-bar' >
+asdasd
+         </div>
+         <div className='basis-1/2 w-fit    board'  >
+          <div className=' w-full h-full ' style={{backgroundImage:'url("./images/chessboard-02.png")',backgroundRepeat:'no-repeat', backgroundPosition:'center' , backgroundSize:'auto 100%'}} >
+
+          </div>
+asadsa
+         </div>
+         <div className='basis-1/4 right-bar' >
+asdas 
+         </div>
+      </div>
     </div>
   );
 }
